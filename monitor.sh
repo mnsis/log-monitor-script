8243a80d39f84a738a0153f9307c0e37752daa2f
MNSIS <107256881+mnsis@users.noreply.github.com>
2024-07-20 23:56:39 +0300

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -35,6 +35,7 @@ rpc_endpoints=(
     "https://t-airchains.rpc.utsa.tech/"
     "https://airchains-rpc.sbgid.com/"
     "https://airchains-rpc.kubenode.xyz/"
+    "https://airchains-testnet-rpc.itrocket.net/"
 )
 check_interval=30  # Interval to check logs in seconds
 error_duration=$((5 * 60))  # Duration to wait before verifying if error persists in seconds (5 minutes)
